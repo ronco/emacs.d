398197112543a2d5af24fc513f47be1da00dbc6f
ronco <613156+ronco@users.noreply.github.com>
2023-09-22 08:37:37 -0600

    indentation function

--- init.el
+++ init.el
@@ -53,3 +53,27 @@
 (tool-bar-mode -1)
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
 
+;; custom functions
+(defun shift-text (distance)
+  (if (use-region-p)
+      (let ((mark (mark)))
+        (save-excursion
+          (indent-rigidly (region-beginning)
+                          (region-end)
+                          distance)
+          (push-mark mark t t)
+          (setq deactivate-mark nil)))
+    (indent-rigidly (line-beginning-position)
+                    (line-end-position)
+                    distance)))
+
+(defun shift-right (count)
+  (interactive "p")
+  (shift-text count))
+
+(defun shift-left (count)
+  (interactive "p")
+  (shift-text (- count)))
+
+;; key bindings
+;; (global-set-key (kbd "s-[") ')
