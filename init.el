af42464e872221439e29aece6d6d699cdda00672
ronco <613156+ronco@users.noreply.github.com>
2023-10-03 13:23:26 -0600

    copilot with <tab>

--- init.el
+++ init.el
@@ -124,35 +124,40 @@ is available. Useful if you tend to hammer your keys like I do."
 (define-key copilot-mode-map (kbd "M-C-<down>") #'copilot-accept-completion-by-line)
 (define-key global-map (kbd "M-C-<return>") #'rk/copilot-complete-or-accept)
 
-;; (defun rk/copilot-tab ()
-;;   "Tab command that will complet with copilot if a completion is
-;; available. Otherwise will try company, yasnippet or normal
-;; tab-indent."
-;;   (interactive)
-;;   (or (copilot-accept-completion)
-;;       (company-yasnippet-or-completion)
-;;       (indent-for-tab-command)))
+(defun rk/copilot-tab ()
+  "Tab command that will complet with copilot if a completion is
+available. Otherwise will try company, yasnippet or normal
+tab-indent."
+  (interactive)
+  (or (copilot-accept-completion)
+      ;; (company-yasnippet-or-completion)
+      (indent-for-tab-command)))
 
-;; (define-key global-map (kbd "<tab>") #'rk/copilot-tab)
+(define-key global-map (kbd "<tab>") #'rk/copilot-tab)
 
-;; (defun rk/copilot-quit ()
-;;   "Run `copilot-clear-overlay' or `keyboard-quit'. If copilot is
-;; cleared, make sure the overlay doesn't come back too soon."
-;;   (interactive)
-;;   (condition-case err
-;;       (when copilot--overlay
-;;         (lexical-let ((pre-copilot-disable-predicates copilot-disable-predicates))
-;;           (setq copilot-disable-predicates (list (lambda () t)))
-;;           (copilot-clear-overlay)
-;;           (run-with-idle-timer
-;;            1.0
-;;            nil
-;;            (lambda ()
-;;              (setq copilot-disable-predicates pre-copilot-disable-predicates)))))
-;;     (error handler)))
+(defun rk/copilot-quit ()
+  "Run `copilot-clear-overlay' or `keyboard-quit'. If copilot is
+cleared, make sure the overlay doesn't come back too soon."
+  (interactive)
+  (condition-case err
+      (when copilot--overlay
+        (lexical-let ((pre-copilot-disable-predicates copilot-disable-predicates))
+          (setq copilot-disable-predicates (list (lambda () t)))
+          (copilot-clear-overlay)
+          (run-with-idle-timer
+           1.0
+           nil
+           (lambda ()
+             (setq copilot-disable-predicates pre-copilot-disable-predicates)))))
+    (error handler)))
 
-;; (advice-add 'keyboard-quit :before #'rk/copilot-quit)
+(advice-add 'keyboard-quit :before #'rk/copilot-quit)
 (global-copilot-mode)
+
+;; magit
+(add-hook 'magit-mode-hook
+          (lambda () (local-set-key (kbd "<tab>") #'magit-section-toggle)))
+
 
 
 ;; Diminish as necessary
